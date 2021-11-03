2b703eb9d362cc27ea64cf7f2bec8e35b99b6287
matheus <matheuscoding@gmail.com>
2021-11-03 13:38:48 -0300

    :art: Melhorar a parte de login e registrar-se

--- src/user/urls.py
+++ src/user/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('sigin/', views.sigin, name='sigin'),
+    path('signin/', views.signin, name='signin'),
     path('check_register/', views.check_register, name='check_register'),
+    path('check_signin/', views.check_signin, name='check_signin'),
+    path('logout/', views.logout, name='logout'),
 ]
--- src/user/views.py
+++ src/user/views.py
@@ -4,11 +4,16 @@ from .models import User
 import hashlib
 
 def register(request):
+    if request.session.get('user'):
+        return redirect('/home/')
     status = request.GET.get('status')
     return render(request, 'register.html', {'status':status})
 
-def sigin(request):
-    return render(request, 'sigin.html')
+def signin(request):
+    if request.session.get('user'):
+        return redirect('/home/')
+    status = request.GET.get('status')
+    return render(request, 'signin.html', {'status': status})
 
 
 def check_register(request):
@@ -22,7 +27,7 @@ def check_register(request):
     if len(user) > 0:
         return redirect('/auth/register/?status=1')
     
-    if len(name.strip()) == 0 or len(email.strip()) == 0:
+    if len(name.strip()) == 0 or len(email.strip()) == 0 or len(telephone.strip()) == 0:
         return redirect('/auth/register/?status=2')
     
     if len(password) < 8:
@@ -38,3 +43,20 @@ def check_register(request):
         return redirect('/auth/register/?status=0')
     except:
         return redirect('/auth/register/?status=4')
+
+def check_signin(request):
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    password = hashlib.sha256(password.encode()).hexdigest()
+    users = User.objects.filter(email = email).filter(password = password)
+
+    if len(users) == 0:
+        return redirect('/auth/signin/?status=1')
+    elif len(users) > 0:
+        request.session['user'] = users[0].id
+        return redirect('/home/')
+
+def logout(request):
+    request.session.flush()
+    return redirect('/auth/signin/')
+
