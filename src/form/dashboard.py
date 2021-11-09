a5ffc1af8c62aeb80089ca5f6123181874daa57d
Renata Gabriela <69400042+renatagsr@users.noreply.github.com>
2021-11-08 23:07:35 -0300

    Mudanças nos gráficos deixando mais limpo

--- src/form/dashboard.py
+++ src/form/dashboard.py
@@ -1,36 +1,49 @@
 import dash
-import dash_core_components as dcc
+import dash_core_components as dcc, Dropdown
 import dash_html_components as html
 import plotly.graph_objs as go
 import dash_bootstrap_components as dbc
-from dash_html_components import Div, H1, H2, Img, P, Header, Span
+from dash_html_components import Div, H1, H2, H3, H4
+import pandas as pd
+import numpy as np
+from dash.dependencies import Input, Output
 
 from random import random #APAGAR no oficial
-
 
 from django_plotly_dash import DjangoDash
 
 app = DjangoDash('SimpleExample')
 
 
-def graph(x, y, ministerio, color):
+def graph(x, y1, y2, color1, color2):
     data = go.Bar(
     x = x,
-    y = y, 
-    marker = {'color': color},
-    text = y,
+    y = y1, 
+    marker = {'color': color1},
+    #text = y1,
     textfont = {'size': 14},
-    textposition = 'outside'
+    textposition = 'outside',
+    name ='Resultado sozinho'
+    )
+
+    data2 = go.Bar(
+    x = x,
+    y = y2, 
+    marker = {'color': color2},
+    #text = y2,
+    textfont = {'size': 14},
+    textposition = 'outside',
+    name ='Resultado com o do grupo'
     )
 
     configuracoes_layout = go.Layout(
-        title = {
-            'text': f'Rank para o {ministerio}',
-            'font': {'size': 20},
-            'x': 0.5,
-            'xanchor': 'center',
-            'font': {'color': '#bdc3c7'}
-        },
+        #title = {
+            #'text': f'Rank para o {ministerio}',
+            #'font': {'size': 20},
+            #'x': 0.5,
+            #'xanchor': 'center',
+            #'font': {'color': '#bdc3c7'}
+        #},
         xaxis = {
             'title': 'Países',
             'titlefont': {'color': '#bdc3c7'},
@@ -43,135 +56,100 @@ def graph(x, y, ministerio, color):
         template='plotly_white'
     )
 
-    fig = go.Figure(data=data, layout=configuracoes_layout)
+    fig = go.Figure(data=[data, data2], layout=configuracoes_layout)
     return fig
 
 paises = ['Argentina', 'Australia', 'Brasil', 'Canadá', 'China', 'França', 'Alemanha', 'India', 'Indonésia', 'Italia', 'Japão', 'Coreia do Sul', 'México']
 
 #Resultados
-resultados_dos_usuarios_agricultura = [random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100]
-resultados_dos_usuarios_educacao = [random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100]
-resultados_dos_usuarios_clima= [random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100]
-resultados_dos_usuarios_saude= [random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100]
-resultados_dos_usuarios_ciencia = [random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100]
-resultados_dos_usuarios_desenvolvimento = [random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100]
-resultados_dos_usuarios_banco_central = [random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100]
-resultados_dos_usuarios_economia = [random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100]
+total = [random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100]
+total_2 = [random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100]
 
 
-fig_agricultura = graph(paises, resultados_dos_usuarios_agricultura, 'ministério da Agricultura e Desenvolvimento rural', '#b86224')
-fig_educacao = graph(paises, resultados_dos_usuarios_educacao, 'ministério da Educação', '#6c8eb0')
-fig_clima = graph(paises, resultados_dos_usuarios_clima, 'ministério do Meio Ambiente', '#e99e2a')
-fig_saude = graph(paises, resultados_dos_usuarios_saude, 'ministério da Saúde', '#c3aca0')
-fig_ciencia = graph(paises, resultados_dos_usuarios_ciencia, 'ministério da Infraestreutura, Ciência e Tecnologia', '#1b1505')
-fig_desenvolvimento = graph(paises, resultados_dos_usuarios_desenvolvimento, 'ministério do Desenvolvimento', '#db6e25')
-fig_banco_central = graph(paises, resultados_dos_usuarios_banco_central, 'banco central', '#989998')
-fig_economia = graph(paises, resultados_dos_usuarios_economia, 'ministério da economia', '#e99e2a')
+fig_total = graph(paises, total, total_2, '#e99e2a', '#bdc3c7')
 
 app.layout = html.Div([
         H1('Dashboard'),
         
-        #Agricultura
-        H2('Ministério da Agricultura e Desenvolvimento rural'),
+        #Ranking por ministério
         dbc.Row([
             dbc.Col(
                 Div([
+                    Dropdown(
+                        id = 'meu_dropdown',
+                        value='Argentina',
+                        options=[
+                            {'label': x, 'value': x} for x in paises
+                        ]
+                    ),
+                    #H4('Ministério da Agricultura e Desenvolvimento rural'),
                     dcc.Graph(
                         id = 'Gráfico ministério da agricultura e desenvolvimento',
                         config = {'displayModeBar': False},
-                        figure = fig_agricultura
-                        )
-                    ]), 
-                width=12)]),
-
-        #Educação
-        H2('Ministério da Educação'),
-        dbc.Row([
-            dbc.Col(
-                Div([
-                    dcc.Graph(
-                        id = 'Gráfico ministério da educação',
-                        config = {'displayModeBar': False},
-                        figure = fig_educacao
+                        figure = {}
                         )
                     ]), 
                 width=12)]),
         
-        #Clima
-        H2('Ministério do Meio Ambiente'),
+        #Ranking dos países
+        H4('Ranking Geral'),
         dbc.Row([
             dbc.Col(
                 Div([
                     dcc.Graph(
-                        id = 'Gráfico ministério do Meio Ambiente',
+                        id = 'Gráfico total',
                         config = {'displayModeBar': False},
-                        figure = fig_clima
+                        figure = fig_total
                         )
                     ]), 
                 width=12)]),
 
-        #Saúde
-        H2('Ministério da Saúde'),
-        dbc.Row([
-            dbc.Col(
-                Div([
-                    dcc.Graph(
-                        id = 'Gráfico ministério da saúde',
-                        config = {'displayModeBar': False},
-                        figure = fig_saude
-                        )
-                    ]), 
-                width=12)]),
-
-        #Ciência
-        H2('Ministério da Infraestreutura, Ciência e Tecnologia'),
-        dbc.Row([
-            dbc.Col(
-                Div([
-                    dcc.Graph(
-                        id = 'Gráfico ministério do Infraestreutura, Ciência e Tecnologia',
-                        config = {'displayModeBar': False},
-                        figure = fig_ciencia
-                        )
-                    ]), 
-                width=12)]),
         
-        #Desenvolvimento
-        H2('Banco Central'),
-        dbc.Row([
-            dbc.Col(
-                Div([
-                    dcc.Graph(
-                        id = 'Gráfico do ministério do Desenvolvimento',
-                        config = {'displayModeBar': False},
-                        figure = fig_desenvolvimento
-                        )
-                    ]), 
-                width=12)]),
-        
-        #Banco Central
-        H2('Banco Central'),
-        dbc.Row([
-            dbc.Col(
-                Div([
-                    dcc.Graph(
-                        id = 'Gráfico banco central',
-                        config = {'displayModeBar': False},
-                        figure = fig_banco_central
-                        )
-                    ]), 
-                width=12)]),
-
-        #Economia
-        H2('Ministério da Economia'),
-        dbc.Row([
-            dbc.Col(
-                Div([
-                    dcc.Graph(
-                        id = 'Gráfico ministério da Economia',
-                        config = {'displayModeBar': False},
-                        figure = fig_economia
-                        )
-                    ]), 
-                width=12)]),
 ])
+
+@app.callback(
+    Output('Gráfico ministério da agricultura e desenvolvimento', 'figure'),
+    Input('meu_dropdown', 'value'),
+)
+
+def my_callback(nome_pais):
+    if nome_pais == None:
+        nome_pais = 'Michelle'
+    
+    ministerios = ['Agricultura', 'Educação', 'Meio Ambiente', 'Saúde', 'Ciência', 'Desenvolvimento', 'Banco Central', 'Economia']
+    paises = ['Argentina', 'Australia', 'Brasil', 'Canadá', 'China', 'França', 'Alemanha', 'India', 'Indonésia', 'Italia', 'Japão', 'Coreia do Sul', 'México', 'Rússia']
+
+    df = pd.DataFrame(np.random.randint(0,100,size=(140, 8)), columns=ministerios)
+    df['paises'] = paises * 10
+
+    data = go.Bar(
+        x =  df[df['paises'] == 'Argentina'].sum().index[:-1],
+        y = df[df['paises'] == 'Argentina'].sum(), 
+        marker = {'color': '#b86224'},
+        #text = y,
+        textfont = {'size': 14},
+        textposition = 'outside'
+    )
+
+    configuracoes_layout = go.Layout(
+        title = {
+            'text': f'Rank para {nome_pais}',
+            'font': {'size': 20},
+            'x': 0.5,
+            'xanchor': 'center',
+            'font': {'color': '#bdc3c7'}
+        },
+        xaxis = {
+            'title': 'Países',
+            'titlefont': {'color': '#bdc3c7'},
+            'tickfont': {'size': 12},
+        },
+        yaxis = {
+            'title': 'Valor para Cada Ministério', 
+            'titlefont': {'color': '#bdc3c7'}
+        },
+        template='plotly_white'
+    )
+
+    fig = go.Figure(data=data, layout=configuracoes_layout)
+    return fig
