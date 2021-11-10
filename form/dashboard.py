7baf0fb24611b7da668f52a0383b3e9f5b23fb1c
renatagsr <renata.gabriela98@gmail.com>
2021-11-10 18:34:59 -0300

    Grafico atualizando automaticamente

--- form/dashboard.py
+++ form/dashboard.py
@@ -19,52 +19,6 @@ from django_plotly_dash import DjangoDash
 
 app = DjangoDash('SimpleExample')
 
-
-def graph(x, y1, y2, color1, color2):
-    data = go.Bar(
-        x=x,
-        y=y1,
-        marker={'color': color1},
-        #text = y1,
-        textfont={'size': 14},
-        textposition='outside',
-        name='Resultado sozinho'
-    )
-
-    data2 = go.Bar(
-        x=x,
-        y=y2,
-        marker={'color': color2},
-        #text = y2,
-        textfont={'size': 14},
-        textposition='outside',
-        name='Resultado com o do grupo'
-    )
-
-    configuracoes_layout = go.Layout(
-        # title = {
-        # 'text': f'Rank para o {ministerio}',
-        # 'font': {'size': 20},
-        # 'x': 0.5,
-        # 'xanchor': 'center',
-        # 'font': {'color': '#bdc3c7'}
-        # },
-        xaxis={
-            'title': 'Países',
-            'titlefont': {'color': '#bdc3c7'},
-            'tickfont': {'size': 12},
-        },
-        yaxis={
-            'title': 'Valor Conquistado no Rodada',
-            'titlefont': {'color': '#bdc3c7'}
-        },
-        template='plotly_white'
-    )
-
-    fig = go.Figure(data=[data, data2], layout=configuracoes_layout)
-    return fig
-
-
 paises = sorted([i.country for i in User.objects.all(
 ) if i.country is not None])
 
@@ -74,52 +28,6 @@ formularios = sorted([i.name for i in Form.objects.all()])
 ministerios = ['paises', 'Agricultura', 'Educação', 'Meio Ambiente',
                'Saúde', 'Ciência', 'Desenvolvimento', 'Banco Central', 'Economia']
 
-tuples = []
-for i in Answer.objects.all():
-    leader = i.leader
-    tuples.append([leader.country, i.result_agricultura, i.result_educacao, i.result_ambiente,
-                   i.result_saude, i.result_infraestrutura, i.result_desenvolvimento,
-                   i.result_bancoCentral, i.result_economia])
-
-df = pd.DataFrame(tuples, columns=ministerios)
-
-somas = {}
-for i in df["paises"]:
-    soma = 0
-    for j in df[df["paises"] == i].iloc[:, 1:].sum():
-        soma += j
-    somas[i] = soma
-
-# Resultados
-
-total = [random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() *
-         100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100, random() * 100]
-
-users = User.objects.all()
-blocos = {}
-for i in users:
-    if i.bloc in blocos.keys():
-        blocos[i.bloc].append(i.country)
-    else:
-        blocos[i.bloc] = [i.country]
-for i in blocos:
-    soma = 0
-    for j in blocos[i]:
-        valor = somas.get(j)
-        if valor:
-            soma += valor
-        else:
-            soma += 0
-    blocos[i] = {"paises": blocos[i],
-                 "soma": soma, "media": soma/len(blocos[i])}
-
-somas_final = {}
-for i in somas:
-    user = User.objects.filter(country=i)[0]
-    somas_final[i] = somas[i] + (0.1 * blocos[user.bloc]['media'])
-
-fig_total = graph(list(somas.keys()), list(somas.values()),
-                  list(somas_final.values()), '#e99e2a', '#bdc3c7')
 
 app.layout = html.Div([
     H4('Ranking por ministério'),
@@ -130,7 +38,7 @@ app.layout = html.Div([
                 Div([
                     Dropdown(
                         id='meu_dropdown',
-                        value='Argentina',
+                        value='',
                         options=[
                             {'label': x, 'value': x} for x in paises
                         ]
@@ -148,6 +56,11 @@ app.layout = html.Div([
                         id='Gráfico ministério da agricultura e desenvolvimento',
                         config={'displayModeBar': False},
                         figure={}
+                    ),
+                    dcc.Interval(
+                        id='interval-component',
+                        interval=1*1000, # in milliseconds
+                        n_intervals=0
                     )
                 ]),
                 width=12)]),
@@ -160,7 +73,12 @@ app.layout = html.Div([
                     dcc.Graph(
                         id='Gráfico total',
                         config={'displayModeBar': False},
-                        figure=fig_total
+                        figure={}
+                    ),
+                    dcc.Interval(
+                        id='interval-component',
+                        interval=1*1000, # in milliseconds
+                        n_intervals=0
                     )
                 ]),
                 width=12)]),
@@ -171,16 +89,16 @@ app.layout = html.Div([
 
 @app.callback(
     Output('Gráfico ministério da agricultura e desenvolvimento', 'figure'),
-    [Input('meu_dropdown', 'value'), Input('my_checklist', 'value')],
+    [Input('meu_dropdown', 'value'), Input('my_checklist', 'value'), Input('interval-component', 'n_intervals')],
 )
-def my_callback(nome_pais, formulario):
+def my_callback(nome_pais, formulario, n):
     if not nome_pais:
         nome_pais = ''
 
     ministerios = ['paises', 'Ano', 'Agricultura', 'Educação', 'Meio Ambiente',
                    'Saúde', 'Ciência', 'Desenvolvimento', 'Banco Central',
                    'Economia']
-
+    
     tuples = []
     for i in Answer.objects.all():
         leader = i.leader
@@ -226,4 +144,96 @@ def my_callback(nome_pais, formulario):
     )
 
     fig = go.Figure(data=data, layout=configuracoes_layout)
+    return fig
+
+@app.callback(
+    Output('Gráfico total', 'figure'),
+    [Input('interval-component', 'n_intervals')],
+)
+
+def my_graph(n):
+    
+    tuples = []
+    for i in Answer.objects.all():
+        leader = i.leader
+        tuples.append([leader.country, i.result_agricultura, i.result_educacao, i.result_ambiente,
+                    i.result_saude, i.result_infraestrutura, i.result_desenvolvimento,
+                    i.result_bancoCentral, i.result_economia])
+
+    df = pd.DataFrame(tuples, columns=ministerios)
+
+    somas = {}
+    for i in df["paises"]:
+        soma = 0
+        for j in df[df["paises"] == i].iloc[:, 1:].sum():
+            soma += j
+        somas[i] = soma
+
+    # Resultados
+
+    users = User.objects.all()
+    blocos = {}
+    for i in users:
+        if i.bloc in blocos.keys():
+            blocos[i.bloc].append(i.country)
+        else:
+            blocos[i.bloc] = [i.country]
+    for i in blocos:
+        soma = 0
+        for j in blocos[i]:
+            valor = somas.get(j)
+            if valor:
+                soma += valor
+            else:
+                soma += 0
+        blocos[i] = {"paises": blocos[i],
+                    "soma": soma, "media": soma/len(blocos[i])}
+
+    somas_final = {}
+    for i in somas:
+        user = User.objects.filter(country=i)[0]
+        somas_final[i] = somas[i] + (0.1 * blocos[user.bloc]['media'])
+    
+
+    data = go.Bar(
+        x=list(somas.keys()),
+        y=list(somas.values()),
+        marker={'color': '#e99e2a'},
+        #text = y1,
+        textfont={'size': 14},
+        textposition='outside',
+        name='Resultado sozinho'
+    )
+
+    data2 = go.Bar(
+        x=list(somas.keys()),
+        y=list(somas_final.values()),
+        marker={'color': '#bdc3c7'},
+        #text = y2,
+        textfont={'size': 14},
+        textposition='outside',
+        name='Resultado com o do grupo'
+    )
+
+    configuracoes_layout = go.Layout(
+        # title = {
+        # 'text': f'Rank para o {ministerio}',
+        # 'font': {'size': 20},
+        # 'x': 0.5,
+        # 'xanchor': 'center',
+        # 'font': {'color': '#bdc3c7'}
+        # },
+        xaxis={
+            'title': 'Países',
+            'titlefont': {'color': '#bdc3c7'},
+            'tickfont': {'size': 12},
+        },
+        yaxis={
+            'title': 'Valor Conquistado no Rodada',
+            'titlefont': {'color': '#bdc3c7'}
+        },
+        template='plotly_white'
+    )
+
+    fig = go.Figure(data=[data, data2], layout=configuracoes_layout)
     return fig
