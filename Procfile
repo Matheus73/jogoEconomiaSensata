1fcb2d19a8c1efa0b89d53a8e9022bec38c9e388
matheus <matheuscoding@gmail.com>
2021-11-10 11:36:03 -0300

    Preparando arquivos de deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn economiaSensata.wsgi
--- form/views.py
+++ form/views.py
@@ -31,8 +31,8 @@ def dashboard(request):
 def create_poll(request):
     user_id = request.session.get('user')
     user = User.objects.get(pk=user_id)
-    form_name = request.POST.get("form_name")
-    golpe = request.POST.get("golpe")
+    form_name = request.POST.get('form_name')
+    golpe = request.POST.get('golpe')
     form = Form.objects.filter(name=form_name)[0]
 
     exists_polls = len(Poll.objects.filter(created_by=user, form=form)) > 0
@@ -58,34 +58,34 @@ def poll(request):
         for i in polls:
             creator = User.objects.get(pk=i.created_by.id)
             is_my = creator.id == user_id
-            if not i.has_open or is_my or user_id in i.votes["votes"]:
+            if not i.has_open or is_my or user_id in i.votes['votes']:
                 if i.pros > i.against:
                     result = True
                 else:
                     result = False
-                final_polls.append({"form": i.form.name, "country": creator.country,
-                                   "active": (i.has_open), "owner": is_my, "coup": i.coup, "can_vote": False, "poll_id": i.id, "result": result})
+                final_polls.append({'form': i.form.name, 'country': creator.country,
+                                   'active': (i.has_open), 'owner': is_my, 'coup': i.coup, 'can_vote': False, 'poll_id': i.id, 'result': result})
             else:
                 final_polls.append(
-                    {"form": i.form.name, "country": creator.country, "can_vote": True, "coup": i.coup,  "owner": is_my, "active": i.has_open, "poll_id": i.id})
+                    {'form': i.form.name, 'country': creator.country, 'can_vote': True, 'coup': i.coup,  'owner': is_my, 'active': i.has_open, 'poll_id': i.id})
 
-        return render(request, 'polls.html', {"polls": final_polls, "forms": [{"value": i.name.replace(" ", "_"), "name": i.name} for i in forms]})
+        return render(request, 'polls.html', {'polls': final_polls, 'forms': [{'value': i.name.replace(' ', '_'), 'name': i.name} for i in forms]})
 
     else:
         return redirect('/auth/signin/?status=2')
 
 
 def check_poll(request):
-    choice = request.POST.get("choice")
-    poll_id = request.POST.get("poll_id")
+    choice = request.POST.get('choice')
+    poll_id = request.POST.get('poll_id')
     poll = Poll.objects.get(pk=poll_id)
-    if choice == "1":
+    if choice == '1':
         poll.pros += 1
     else:
         poll.against += 1
     votes = poll.votes['votes']
-    votes.append(request.session.get("user"))
-    poll.votes = {"votes": votes}
+    votes.append(request.session.get('user'))
+    poll.votes = {'votes': votes}
     poll.save()
     return redirect('/?status=2')
 
@@ -97,50 +97,50 @@ def render_form(request, id):
             return redirect('/?status=1')
         status = request.GET.get('status')
         return render(request, 'form.html',
-                      {"questions": form.questions['questions'], "name": form.name, "id": id, "status": status})
+                      {'questions': form.questions['questions'], 'name': form.name, 'id': id, 'status': status})
     else:
         return redirect('/auth/signin/?status=2')
 
 
 def check_form(request):
 
-    json = {"questions": [
+    json = {'questions': [
         {
-            "model": "agricultura",
-            "questions": ["question1"]
+            'model': 'agricultura',
+            'questions': ['question1']
         },
         {
-            "model": "educacao",
-            "questions": ["question2", "question3", "question4", ]
+            'model': 'educacao',
+            'questions': ['question2', 'question3', 'question4', ]
         },
         {
-            "model": "ambiente",
-            "questions": ["question5", "question6", "question7", ]
+            'model': 'ambiente',
+            'questions': ['question5', 'question6', 'question7', ]
         },
         {
-            "model": "saude",
-            "questions": ["question8", ]
+            'model': 'saude',
+            'questions': ['question8', ]
         },
         {
-            "model": "ciencia",
-            "questions": ["question9", ]
+            'model': 'ciencia',
+            'questions': ['question9', ]
         },
         {
-            "model": "desenvolvimento",
-            "questions": ["question10", ]
+            'model': 'desenvolvimento',
+            'questions': ['question10', ]
         },
         {
-            "model": "banco",
-            "questions": ["question11", ]
+            'model': 'banco',
+            'questions': ['question11', ]
         },
         {
-            "model": "economia",
-            "questions": ["question12", "question13", "question14", ]
+            'model': 'economia',
+            'questions': ['question12', 'question13', 'question14', ]
         }
     ]
     }
-    answers = [{"question": f"question{i}", "answer": request.POST.get(
-        f"question{i}")} for i in range(1, 15)]
+    answers = [{'question': f'question{i}', 'answer': request.POST.get(
+        f'question{i}')} for i in range(1, 15)]
 
     user_id = request.session.get('user')
     form_id = request.POST.get('id')
@@ -164,17 +164,17 @@ def check_form(request):
         aproved = False
 
     for i in answers:
-        if i["answer"] == '':
+        if i['answer'] == '':
             return redirect(f'/form/{form_id}/?status=1')
-        elif int(i["answer"]) > 40:
+        elif int(i['answer']) > 40:
             return redirect(f'/form/{form_id}/?status=2')
-        elif int(i["answer"]) > 20 and (not aproved or not poll.coup):
+        elif int(i['answer']) > 20 and (not aproved or not poll.coup):
             return redirect(f'/form/{form_id}/?status=3')
 
     final_json = {}
     for i in json['questions']:
         final_json[i['model']] = [
-            int(j["answer"]) for j in answers if j['question'] in i["questions"]]
+            int(j['answer']) for j in answers if j['question'] in i['questions']]
 
     final_json['agricultura'] = [
         final_json['agricultura'][0]/100 * int(user.money)]
@@ -189,8 +189,8 @@ def check_form(request):
         tmp = final_json[i]
         results[i] = predict_min(tmp, i)
 
-    answer = Answer(form=form, leader=user, choices={"answers": answers}, result_agricultura=results['agricultura'],
-                    result_educacao=results['educacao'], result_ambiente=results['ambiente'], result_saude=results["saude"],
+    answer = Answer(form=form, leader=user, choices={'answers': answers}, result_agricultura=results['agricultura'],
+                    result_educacao=results['educacao'], result_ambiente=results['ambiente'], result_saude=results['saude'],
                     result_infraestrutura=results['ciencia'], result_desenvolvimento=results['desenvolvimento'],
                     result_bancoCentral=results['banco'], result_economia=results['economia'])
     answer.save()
@@ -216,7 +216,7 @@ def home(request):
     if request.session.get('user'):
         status = request.GET.get('status')
         return render(request, 'home.html',
-                      {"status": status})
+                      {'status': status})
     else:
         return redirect('/auth/signin/?status=2')
 
@@ -225,18 +225,18 @@ def about(request):
     if request.session.get('user'):
         status = request.GET.get('status')
         return render(request, 'about.html',
-                      {"status": status})
+                      {'status': status})
     else:
         return redirect('/auth/signin/?status=2')
 
 
 def predict_min(data, name):
-    path = os.path.dirname("../")
-    model = pickle.load(open(f"{path}/models/model_{name}.pkl", 'rb'))
+    path = os.path.dirname('')
+    model = pickle.load(open(f'{path}models/model_{name}.pkl', 'rb'))
     normalizer = pickle.load(
-        open(f"{path}/models/normalizer_{name}.pkl", 'rb'))
+        open(f'{path}models/normalizer_{name}.pkl', 'rb'))
     normalizer_out = pickle.load(
-        open(f"{path}/models/normalizer_{name}_out.pkl", 'rb'))
+        open(f'{path}models/normalizer_{name}_out.pkl', 'rb'))
 
     input = normalizer.transform([data])
 
@@ -250,6 +250,6 @@ def coup(request, id):
     if request.session.get('user'):
         status = request.GET.get('status')
         return render(request, 'coup.html',
-                      {"status": status, "id": id})
+                      {'status': status, 'id': id})
     else:
         return redirect('/auth/signin/?status=2')
